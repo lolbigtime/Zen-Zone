5e8cf2f8a95b1cd5c69ce9c35a93886bef038dd4
Andruw <andruwkllin@gmail.com>
2023-07-12 18:26:27 -0700

    Homeview detailed descriptions

--- Zen Zone/Views/HomeView.swift
+++ Zen Zone/Views/HomeView.swift
@@ -8,11 +8,12 @@
 import SwiftUI
 
 struct HomeView: View {
-    
+    @State private var isPresenting = false
+    @State private var isPresented = false
+
     var weeklies = activities.filter { $0.type == "Weekly" }
     var dailies = activities.filter { $0.type == "Daily" }
-    
-    
+
     var body: some View {
         ZStack {
             Color("Background").ignoresSafeArea()
@@ -21,9 +22,7 @@ struct HomeView: View {
             }
         }
     }
-    
-    
-    
+
     var content: some View {
         VStack(alignment: .leading, spacing: 0) {
             Text("Categories")
@@ -33,9 +32,7 @@ struct HomeView: View {
             
             ScrollView(.horizontal, showsIndicators: false) {
                 HStack {
-                    ForEach(options) { option in
-                        CCard(option: option)
-                    }
+                    ForEach(options) { option in CCard(option: option)}
                 }
                 .padding(.leading, 20)
                 .padding(.bottom, 10)
@@ -46,12 +43,46 @@ struct HomeView: View {
                 .frame(maxWidth: .infinity, alignment: .leading)
                 .padding(.horizontal, 20)
             ScrollView(.horizontal, showsIndicators: false) {
-                HStack {
-                    ForEach(weeklies) { activity in
-                        HCard(activity: activity)
+                ZStack {
+                    ForEach(weeklies) { activity in HCard(activity: activity)}
+                    Button("Start") {
+                        isPresenting.toggle()
                     }
-                }
-                .padding(20)
+                    .padding(.top, 200)
+                    .padding(.trailing, 0)
+                    .foregroundColor(.white)
+                    .fullScreenCover(isPresented: $isPresenting,
+                                     onDismiss: didDismiss) {
+                        VStack {
+                            Text("Practicing Gratitude")
+                                .foregroundColor( .white)
+                                .font(.bold(.largeTitle)())
+                            Spacer()
+                                .frame(maxHeight:30)
+                            Text("One activity that can help improve your mental health is practicing gratitude. Taking a few minutes each day to reflect on the things you are grateful for can help shift your focus to positive experiences and promote a more optimistic outlook.")
+                                .font(.body)
+                                .multilineTextAlignment(.center)
+                                .frame(maxWidth: 300)
+                                .padding(.leading, 20)
+                                .padding(.trailing, 20)
+                            Spacer()
+                                .frame(maxHeight:30)
+                            Text("**3 Points - Weekly activity**")
+                                .customFont(.body, fontSize: 20)
+                            Spacer()
+                                .frame(maxHeight:40)
+                            Text("Tap to Close")
+                        }
+                        .onTapGesture {
+                            isPresenting.toggle()
+                        }
+                        .foregroundColor(.white)
+                        .frame(maxWidth: .infinity,
+                               maxHeight: .infinity)
+                        .background(Color.blue)
+                        .ignoresSafeArea(edges: .all)
+                    }                }
+                .padding(.leading, 20)
                 .padding(.bottom, 10)
             }
             
@@ -60,15 +91,57 @@ struct HomeView: View {
                 .padding(.horizontal, 20)
             
             ScrollView(.horizontal, showsIndicators: false) {
-                VStack {
+                ZStack {
                     ForEach(dailies) { activity in
                         VCard(activity: activity)
+                    Button("Start") {
+                            isPresented.toggle()
+                        }
+                        .padding(.top, 200)
+                        .padding(.trailing, 0)
+                        .foregroundColor(.white)
+                        .fullScreenCover(isPresented: $isPresented,
+                                         onDismiss: completeDismiss) {
+                            VStack {
+                                Text("Mindful Breathing")
+                                    .foregroundColor( .white)
+                                    .font(.bold(.largeTitle)())
+                                Spacer()
+                                    .frame(maxHeight:30)
+                                Text("Practice mindful breathing. This simple practice can help promote relaxation, reduce stress, and improve overall well-being. Encourage yourself to practice mindful breathing whenever you feel overwhelmed or need a quick break during the day.")
+                                    .font(.body)
+                                    .multilineTextAlignment(.center)
+                                    .frame(maxWidth: 300)
+                                    .padding(.leading, 20)
+                                    .padding(.trailing, 20)
+                                Spacer()
+                                    .frame(maxHeight:30)
+                                Text("1 Point - Daily activity")
+                                    .customFont(.title, fontSize: 20)
+                                Spacer()
+                                    .frame(maxHeight:40)
+                                Text("Tap to Close")
+                            }
+                            .onTapGesture {
+                                isPresented.toggle()
+                            }
+                            .foregroundColor(.white)
+                            .frame(maxWidth: .infinity,
+                                   maxHeight: .infinity)
+                            .background(Color.pink)
+                            .ignoresSafeArea(edges: .all)                    }
                     }
                 }
                 .padding(20)
                 .padding(.bottom, 10)
             }
         }
+    }
+    func didDismiss() {
+        // Handle the dismissing action.
+    }
+    func completeDismiss() {
+        // Handle the dismissing action.
     }
 }
 
