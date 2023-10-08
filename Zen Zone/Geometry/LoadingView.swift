2d37f5fa4bd49357ebd89c5e17d6b089ff4499fa
lolbigtime <pooplolbigtimetroll@gmail.com>
2023-10-07 22:04:36 -0700

    Merge pull request #2 from lolbigtime/Tai's-Branch-(DO-NOT-TOUCH)
    Tai's branch (do not touch)

---
+++ Zen Zone/Geometry/LoadingView.swift
@@ -0,0 +1,18 @@
+//
+//  LoadingView.swift
+//  Zen Zone
+//
+//  Created by Tai Wong on 10/7/23.
+//
+
+import SwiftUI
+
+struct LoadingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LoadingView()
+}
---
+++ Zen Zone/Models/UserModel.swift
@@ -0,0 +1,8 @@
+//
+//  UserModel.swift
+//  Zen Zone
+//
+//  Created by Tai Wong on 10/5/23.
+//
+
+import Foundation
