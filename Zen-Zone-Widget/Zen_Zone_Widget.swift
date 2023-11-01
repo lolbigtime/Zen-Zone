299ca88d1a638532f3ac9c2b4ef7aa9d8471b1ec
Andruw <andruwkllin@gmail.com>
2023-10-31 18:12:57 -0700

    Zen Zone Widget, displays daily / weekly activity and its type

--- Zen-Zone-Widget/Zen_Zone_Widget.swift
+++ Zen-Zone-Widget/Zen_Zone_Widget.swift
@@ -7,8 +7,10 @@
 
 import WidgetKit
 import SwiftUI
-
+import Foundation
 struct Provider: TimelineProvider {
+   
+
     func placeholder(in context: Context) -> SimpleEntry {
         SimpleEntry(date: Date(), emoji: "😀")
     }
@@ -40,17 +42,28 @@ struct SimpleEntry: TimelineEntry {
 }
 
 struct Zen_Zone_WidgetEntryView : View {
+    var activity: activitySection
     var entry: Provider.Entry
-
     var body: some View {
-        VStack {
-            Text("Time:")
-            Text(entry.date, style: .time)
+            VStack (alignment: .leading, spacing: 8){
+                Text(activity.title)
+                    .font(.largeTitle)
+                    .frame(maxWidth: 210, alignment: .leading)
+                HStack{
+                    Text("Activity Type: ")
+                    Text(activity.type)
+                }
 
-            Text("Emoji:")
-            Text(entry.emoji)
+            }
+            .foregroundColor(.blue)
+            .padding(30)
+            .frame(width: 300, height: 240)
+            .background(.linearGradient(colors: [.white, .white.opacity(0.5)], startPoint: .topLeading, endPoint: .bottomTrailing))
+            .mask(RoundedRectangle(cornerRadius: 30, style: .continuous))
+            .shadow(color: .white.opacity(0.3), radius: 8, x: 0, y: 12)
+            .shadow(color: .white.opacity(0.3), radius: 2, x: 0, y: 1)
         }
-    }
+    
 }
 
 struct Zen_Zone_Widget: Widget {
@@ -58,19 +71,29 @@ struct Zen_Zone_Widget: Widget {
 
     var body: some WidgetConfiguration {
         StaticConfiguration(kind: kind, provider: Provider()) { entry in
-            if #available(iOS 17.0, *) {
-                Zen_Zone_WidgetEntryView(entry: entry)
-                    .containerBackground(.fill.tertiary, for: .widget)
-            } else {
-                Zen_Zone_WidgetEntryView(entry: entry)
-                    .padding()
-                    .background()
-            }
+            Zen_Zone_WidgetEntryView(activity: activities[0], entry: entry)
+                .padding()
+                .background(Color.clear) // Use the appropriate background color
         }
         .configurationDisplayName("My Widget")
         .description("This is an example widget.")
     }
 }
+struct activitySection: Identifiable {
+    var id = UUID()
+    var title: String
+    var points: Int
+    var caption: String
+    var type: String
+
+}
+
+var activities = [
+    activitySection(title: "Practicing Gratitude", points: 3, caption: "One activity that can help improve your mental health is practicing gratitude. Taking a few minutes each day to reflect on the things you are grateful for can help shift your focus to positive experiences and promote a more optimistic outlook.", type: "Weekly"),
+    activitySection(title: "Practicing Gratitude", points: 3, caption: "One activity that can help improve your mental health is practicing gratitude. Taking a few minutes each day to reflect on the things you are grateful for can help shift your focus to positive experiences and promote a more optimistic outlook.", type: "Weekly"),
+    activitySection(title: "Mindful Breathing", points: 1, caption: "Practice mindful breathing. This simple practice can help promote relaxation, reduce stress, and improve overall well-being. Encourage yourself to practice mindful breathing whenever you feel overwhelmed or need a quick break during the day.", type: "Daily")
+]
+
 
 #Preview(as: .systemSmall) {
     Zen_Zone_Widget()
